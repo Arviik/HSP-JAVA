512dc152b1ecadc76297336106257dc8af595bb5
Arviik <94380788+Arviik@users.noreply.github.com>
2023-02-12 17:44:59 +0100

    feat: ajout de la class Fournisseur

---
+++ src/main/java/com/hspjava/modele/Fournisseur.java
@@ -0,0 +1,13 @@
+package com.hspjava.modele;
+
+public class Fournisseur {
+    private String nom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
