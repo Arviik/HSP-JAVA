eb7bdaa3f8242ef4687d2e5ddb1bbc4606da480e
BENZEKKI_Ta <benzekkitalal@gmail.com>
2023-02-14 12:00:10 +0100

    feat: ajout du squelette du contrôleur secrétaire

--- src/main/java/com/hspjava/controller/SecretaireController.java
+++ src/main/java/com/hspjava/controller/SecretaireController.java
@@ -1,5 +1,9 @@
 package com.hspjava.controller;
 
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXDatePicker;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -10,4 +14,48 @@ public class SecretaireController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    private MFXTextField adresseField;
+
+    @FXML
+    private MFXDatePicker dateField;
+
+    @FXML
+    private MFXTextField descriptionSymptomeField;
+
+    @FXML
+    private MFXTextField emailField;
+
+    @FXML
+    private MFXTextField heureField;
+
+    @FXML
+    private MFXTextField nomField;
+
+    @FXML
+    private MFXComboBox<?> numGraviteField;
+
+    @FXML
+    private MFXTextField numSecuriteSocialField;
+
+    @FXML
+    private MFXComboBox<?> patientField;
+
+    @FXML
+    private MFXTextField prenomField;
+
+    @FXML
+    private MFXTextField telephoneField;
+
+    @FXML
+    void onSubmitDossier() {
+
+    }
+
+    @FXML
+    void onSubmitPatient() {
+
+    }
+
 }
