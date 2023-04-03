bdf0e3ce20cc2bf7bf4dede468cfcd363b9a2d58
Arviik <94380788+arviik@users.noreply.github.com>
2023-04-03 15:37:23 +0200

    feat: modification de JMailer afin de supporter sa configuration à partir d'un fichier ini

--- src/main/java/com/hspjava/services/mailer/JMailer.java
+++ src/main/java/com/hspjava/services/mailer/JMailer.java
@@ -1,7 +1,7 @@
 package com.hspjava.services.mailer;
 
+import com.hspjava.services.config.Config;
 import jakarta.mail.*;
-import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 
@@ -9,32 +9,30 @@ import java.util.Properties;
 
 public class JMailer {
 
-    public static void sendmail(String mail, String code) throws MessagingException {
-        System.out.println("Preparation du mail");
-        Properties props = new Properties();
+    private JMailer() {
+    }
 
+    public static void sendmail(String mail, String code) throws MessagingException {
+        Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
-
-        String myAccountEmail = "***REMOVED***";
-        String myPassword = "***REMOVED***";
-
-        Session session=Session.getInstance(props, new Authenticator() {
+        String emailAccount = Config.get("emailAccount", String.class);
+        String emailPassword = Config.get("emailPassword", String.class);
+        Session session = Session.getInstance(props, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(myAccountEmail, myPassword);
+                return new PasswordAuthentication(emailAccount, emailPassword);
             }
         });
-
-        Message message = prepareMessage(session, myAccountEmail, mail, code);
-        System.out.println(mail);
+        Message message = prepareMessage(session, emailAccount, mail, code);
+        assert message != null;
         Transport.send(message);
-        System.out.println("Mail envoyé");
     }
-    private static Message prepareMessage(Session session, String myAccountEmail, String mail, String text){
-        try{
+
+    private static Message prepareMessage(Session session, String myAccountEmail, String mail, String text) {
+        try {
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(myAccountEmail));
             InternetAddress[] internetAddresses = new InternetAddress[1];
@@ -43,10 +41,9 @@ public class JMailer {
             message.setSubject("Test");
             message.setText(text);
             return message;
-        } catch (AddressException e) {
-            throw new RuntimeException(e);
         } catch (MessagingException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+        return null;
     }
 }
